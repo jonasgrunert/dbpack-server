866d6ef37b69fff7b8c621363a10f35062fd43d7
jonasgrunert <jonas@grunert-berlin.de>
2019-12-03 08:18:16 +0100

    Changes to the state machine work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import WebSocket from 'socket.io';
-import fs from 'fs';
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import WebSocket from "socket.io";
+import fs from "fs";
 
 import {
   GetTableMessage,
@@ -9,18 +9,18 @@ import {
   ExecuteSqlMessage,
   SaveFileMessage,
   DeployMessage,
-  TestMessage,
-} from './Messages';
-import { createMachine } from './statemachine';
-import { setup, getAllFromTable, executeSql, executeTest } from './database';
-import { Connection } from 'oracledb';
-import { saveToFile, deployFile } from './filemanager';
+  TestMessage
+} from "./Messages";
+import { createMachine } from "./statemachine";
+import { setup, getAllFromTable, executeSql, executeTest } from "./database";
+import { Connection } from "oracledb";
+import { saveToFile, deployFile } from "./filemanager";
 
 function handler(_req: IncomingMessage, res: ServerResponse) {
-  fs.readFile(__dirname + '/index.html', function(err, data) {
+  fs.readFile(__dirname + "/index.html", function(err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end("Error loading index.html");
     }
     res.writeHead(200);
     res.end(data);
@@ -36,52 +36,51 @@ async function handleAsync<ReturnType>(
   socket: WebSocket.Socket,
   ...args: Array<any>
 ): Promise<ReturnType | false> {
-  if (!machine.isActive()) {
-    socket.emit('busy');
+  if (machine.isPending()) {
+    socket.emit("busy");
     return false;
   }
   machine.setPending();
   try {
     const r = await f(args);
     machine.success();
-    socket.emit('success');
-    socket.emit('result', r);
+    socket.emit("success");
+    socket.emit("result", r);
     return r;
   } catch (e) {
     machine.failure();
-    socket.emit('failure', e);
+    socket.emit("failure", e.message);
     return false;
   }
 }
 
-io.on('connection', function(socket) {
+io.on("connection", function(socket) {
   let connection: false | Connection = false;
   let connectionString: false | string = false;
   const machine = createMachine();
-  console.log('Connetced');
-  socket.on('state', () => socket.emit('state', machine.getState()));
-  socket.on('options', async (data: SetOptionsMessage) => {
+  socket.on("state", () => socket.emit("state", machine.getState()));
+  socket.on("options", async (data: SetOptionsMessage) => {
     const conn = await handleAsync(setup, machine, socket, data);
     if (conn) {
       connection = conn;
       connectionString = data.connectionString!;
     }
   });
-  socket.on('save', (data: SaveFileMessage) => {
+  socket.on("save", (data: SaveFileMessage) => {
     handleAsync(saveToFile, machine, socket, data.file);
   });
-  socket.on('deploy', (_data: DeployMessage) => {
+  socket.on("deploy", (_data: DeployMessage) => {
     handleAsync(deployFile, machine, socket, connectionString);
   });
-  socket.on('getTable', (data: GetTableMessage) => {
+  socket.on("getTable", (data: GetTableMessage) => {
     handleAsync(getAllFromTable, machine, socket, connection, data.table);
   });
-  socket.on('executeSQL', (data: ExecuteSqlMessage) => {
+  socket.on("executeSQL", (data: ExecuteSqlMessage) => {
     handleAsync(executeSql, machine, socket, connection, data.sql);
   });
-  socket.on('test', (data: TestMessage) => {
+  socket.on("test", (data: TestMessage) => {
     handleAsync(executeTest, machine, socket, data.func, data.parameters);
   });
 });
 
-app.listen(80, () => console.log('Server started'));
+app.listen(80, () => console.log("Server started"));
